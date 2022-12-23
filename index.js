77348498530a43d29ae5729c292c69ed4918c8f2
umuhlygina <113283615+umuhlygina@users.noreply.github.com>
2022-12-23 23:40:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-const app = express()
-const port = process.env.PORT || 8080
-
-app.use(bodyParser.urlencoded({ extended: true }));
+import express from "express"
+import bodyParser from "body-parser"
+const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-      res.render('index.ejs')
-});
+app.get('/', (req, res) =>
+{
+    res.render("index.ejs");
+})
 
 app.post('/', (req, res) =>
-    {
-        const username = req.body.username;
-        res.render('hello.ejs', {username: username});
-    });
-
-app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
+{
+    const username = req.body.username;
+    res.render('hello.ejs', {username: username});
 })
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`listen port: ${port}`))
